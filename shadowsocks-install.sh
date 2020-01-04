7d83b9757abeeb60466dc7f8de07adfcd2cff012
drsanwujiang <drsanwujiang@gmail.com>
2020-01-04 11:38:26 +0800

    Update shaddowsocks-install.sh

--- shadowsocks-install.sh
+++ shadowsocks-install.sh
@@ -34,22 +34,22 @@ function install_shadowsocks() {
 }
 
 function configure_shadowsocks() {
+    port_index=0
+    port_numbers=()
+    port_passwords=()
+
     echo "4) Configure Shadowsocks"
 
-    sudo mkdir /etc/shadowsocks >> /dev/null
+    sudo mkdir -p /etc/shadowsocks >> /dev/null
 
     echo -e '{\n\t"server": "::",\n\t"local_address": "127.0.0.1",\n\t"local_port": 1080,\n\t"port_password":\n\t{' > /etc/shadowsocks/config.json
-
-    port_number=0
-    declare -a port_numbers
-    declare -a port_passwords
 
     while true
     do
         read -p "Input a port number (input 0 to finish): " port
 
         if [ $port -eq 0 ]; then
-            if [ $port_number -eq 0 ]; then
+            if [ $port_index -eq 0 ]; then
                 echo "Error: please input at least 1 port number!"
             else
                 break
@@ -57,27 +57,27 @@ function configure_shadowsocks() {
         elif [ $port -ge 1 ] && [ $port -le 65535 ]; then
             read -p "Input password of port ${port}: " password
 
-            port_numbers[port_number]=port
-            port_passwords[port_number]=password
+            port_numbers+=($port)
+            port_passwords+=($password)
 
-            let port_number++
+            let port_index++
         else
             echo "Error: port number must be in 1 ~ 65535"
             continue
         fi
     done
 
-    for ((i=0; i<=port_number; i++))
+    for ((i=0; i<port_index; i++))
     do
-        echo -e -n "\n\t\t\"${port_numbers[i]}\": \"${port_passwords[i]}\"" >> /etc/shadowsocks/config.json
+        echo -e -n "\n\t\t\"${port_numbers[$i]}\": \"${port_passwords[$i]}\"" >> /etc/shadowsocks/config.json
 
-        if [ $i != port_number ]; then
-            echo -n "," >> /etc/shadowsocks/config.json
+        if [ $i != $port_index ]; then
+            echo "," >> /etc/shadowsocks/config.json
         fi
     done
 
     echo -e '\t},\n\t"timeout": 300,\n\t"method": "aes-256-gcm",\n\t"fast_open": true' >> /etc/shadowsocks/config.json
-    echo -n "}" >> /etc/shadowsocks/config.json
+    echo "}" >> /etc/shadowsocks/config.json
 
     echo "Done"
 }
@@ -85,17 +85,19 @@ function configure_shadowsocks() {
 function configure_systemd() {
     echo "5) Configure Systemd"
 
-    echo "[Unit]" > /etc/systemd/system/shadowsocks-server.service
-    echo "Description=Shadowsocks Server" >> /etc/systemd/system/shadowsocks-server.service
-    echo "After=network.target" >> /etc/systemd/system/shadowsocks-server.service
-    echo "" >> /etc/systemd/system/shadowsocks-server.service
-    echo "[Service]" >> /etc/systemd/system/shadowsocks-server.service
-    echo "ExecStartPre=/bin/sh -c 'ulimit -n 51200'" >> /etc/systemd/system/shadowsocks-server.service
-    echo "ExecStart=/usr/local/bin/ssserver -c /etc/shadowsocks/config.json" >> /etc/systemd/system/shadowsocks-server.service
-    echo "Restart=on-abort" >> /etc/systemd/system/shadowsocks-server.service
-    echo "" >> /etc/systemd/system/shadowsocks-server.service
-    echo "[Install]" >> /etc/systemd/system/shadowsocks-server.service
-    echo -n "WantedBy=multi-user.target" >> /etc/systemd/system/shadowsocks-server.service
+    cat > /etc/systemd/system/shadowsocks-server.service << EOF
+    [Unit]
+    Description=Shadowsocks Server
+    After=network.target
+
+    [Service]
+    ExecStartPre=/bin/sh -c 'ulimit -n 51200'
+    ExecStart=/usr/local/bin/ssserver -c /etc/shadowsocks/config.json
+    Restart=on-abort
+
+    [Install]
+    WantedBy=multi-user.target
+EOF
 
     sudo systemctl enable shadowsocks-server >> /dev/null
 
@@ -123,28 +125,30 @@ function configure_bbr() {
 function optimize_throughput() {
     echo "7) Optimize Throughput"
 
-    echo "fs.file-max = 51200" > /etc/sysctl.d/local.conf
-    echo "net.core.rmem_max = 67108864" >> /etc/sysctl.d/local.conf
-    echo "net.core.wmem_max = 67108864" >> /etc/sysctl.d/local.conf
-    echo "net.core.rmem_default = 65536" >> /etc/sysctl.d/local.conf
-    echo "net.core.wmem_default = 65536" >> /etc/sysctl.d/local.conf
-    echo "net.core.netdev_max_backlog = 4096" >> /etc/sysctl.d/local.conf
-    echo "net.core.somaxconn = 4096" >> /etc/sysctl.d/local.conf
-    echo "" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_tw_recycle = 0" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.ip_local_port_range = 10000 65000" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_max_syn_backlog = 4096" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_wmem = 4096 65536 67108864" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.d/local.conf
-    echo "" >> /etc/sysctl.d/local.conf
-    echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.d/local.conf
+    cat > /etc/sysctl.conf << EOF
+    fs.file-max = 51200
+    net.core.rmem_max = 67108864
+    net.core.wmem_max = 67108864
+    net.core.rmem_default = 65536
+    net.core.wmem_default = 65536
+    net.core.netdev_max_backlog = 4096
+    net.core.somaxconn = 4096
+
+    net.ipv4.tcp_syncookies = 1
+    net.ipv4.tcp_tw_reuse = 1
+    net.ipv4.tcp_tw_recycle = 0
+    net.ipv4.tcp_fin_timeout = 30
+    net.ipv4.tcp_keepalive_time = 1200
+    net.ipv4.ip_local_port_range = 10000 65000
+    net.ipv4.tcp_max_syn_backlog = 4096
+    net.ipv4.tcp_max_tw_buckets = 5000
+    net.ipv4.tcp_fastopen = 3
+    net.ipv4.tcp_rmem = 4096 87380 67108864
+    net.ipv4.tcp_wmem = 4096 65536 67108864
+    net.ipv4.tcp_mtu_probing = 1
+
+    net.ipv4.tcp_congestion_control = bbr
+EOF
 
     sysctl --system >> /dev/null
 
@@ -155,7 +159,7 @@ function start_shadowsocks() {
     echo "8) Start Shadowsocks"
 
     sudo systemctl daemon-reload >> /dev/null
-    sudo systemctl start shadowsocks-server >> /dev/null
+    sudo systemctl restart shadowsocks-server >> /dev/null
 
     echo "Done"
 }
@@ -167,14 +171,14 @@ function info_display() {
     echo " Port  * Password"
     echo "*************************"
 
-    for ((i=0; i<=port_number; i++))
+    for ((i=0; i<port_index; i++))
     do
-        printf " %-6d* %s" port_numbers[i] port_passwords[i]
+        printf " %-6d* %s\n" ${port_numbers[$i]} ${port_passwords[$i]}
     done
 
     echo -e "\nEncryption Method: aes-256-gcm"
 
-    echo "\n===== Just Fly ====="
+    echo -e "\n===== Just Fly ====="
 }
 
 function start_install() {
